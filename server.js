dbbd96ebb5fee7d7e8fec9a85eff24a8507903c8
saver-sketch <saver@maxsaveins.com>
2026-04-08 13:59:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const VERIFY_SID = process.env.TWILIO_VERIFY_SID;
 
 // Email transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
